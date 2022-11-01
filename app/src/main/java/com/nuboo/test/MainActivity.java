f1b965313458e63d986ba83bbb17b0a9392f633d
davideloddeDev <85182339+davideloddeDev@users.noreply.github.com>
2022-11-01 12:39:51 +0100

    test

--- app/src/main/java/com/nuboo/test/MainActivity.java
+++ app/src/main/java/com/nuboo/test/MainActivity.java
@@ -3,12 +3,34 @@ package com.nuboo.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText username, password;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.text);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username1 = username.getText().toString();
+                String password1 = password.getText().toString();
+
+                textView.setText("dati inseriti: \n Username: " + username1 + "; password: " + password1 );
+            }
+        });
     }
 }
